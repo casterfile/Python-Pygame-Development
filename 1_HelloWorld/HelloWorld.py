6d26b75fdc8299ff7b5a5f304cd54ea1e2410c57
Anthony Castor <Anthony Castor>
2017-05-12 11:47:41 +0800

    Upload

---
+++ 1_HelloWorld/HelloWorld.py
@@ -0,0 +1,12 @@
+import kivy
+
+from kivy.app import App
+kivy.require("1.8.0")
+from kivy.uix.label import Label  
+
+class HelloWorldAppKivy(App):
+	def build(self):
+		return Label(text = "Hello World")
+
+if __name__ == '__main__':
+	HelloWorldAppKivy().run()
